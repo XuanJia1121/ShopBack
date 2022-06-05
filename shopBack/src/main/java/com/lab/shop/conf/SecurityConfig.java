7f203019d4b9e4de3f8d07b68d85824d4c1a65e7
XuanJia1121 <xuanjia961121@gmail.com>
2022-06-05 20:45:46 +0800

    fix lab

--- shopBack/src/main/java/com/lab/shop/conf/SecurityConfig.java
+++ shopBack/src/main/java/com/lab/shop/conf/SecurityConfig.java
@@ -1,28 +1,40 @@
 package com.lab.shop.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import com.lab.shop.service.AuthFailService;
 import com.lab.shop.service.AuthSuccessService;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private AuthSuccessService authSuccessService;
 	@Autowired
 	private AuthFailService authFailService;
 
-	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http
-				.antMatcher("/**")
-				.authorizeRequests(authorize -> authorize.anyRequest()
-				.authenticated()).build();
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		 http
+         .authorizeRequests()
+         .antMatchers(HttpMethod.POST, "/api/shop/getAllPro/**").permitAll()
+         .anyRequest().authenticated()
+         .and()
+         .cors()
+         .and()
+         .csrf().disable()
+         .formLogin()
+         .loginProcessingUrl("/login")
+         .usernameParameter("username")
+         .passwordParameter("password")
+         .successHandler(authSuccessService)
+         .failureHandler(authFailService);
+		 
 	}
+	
 }
