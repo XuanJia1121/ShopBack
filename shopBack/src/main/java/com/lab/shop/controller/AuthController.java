9d137d52e469dd6eeddda74dd2612226d004f206
IEAD\2008017 <xuanjia961121@gmail.com>
2022-06-23 17:54:41 +0800

    fix

--- shopBack/src/main/java/com/lab/shop/controller/AuthController.java
+++ shopBack/src/main/java/com/lab/shop/controller/AuthController.java
@@ -1,33 +1,8 @@
 package com.lab.shop.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.lab.shop.service.RedisService;
 
 @RestController
 public class AuthController {
-
-	@Autowired
-	private RedisService redisService;
 	
-	@RequestMapping("/rSet")
-	public void set() {
-		redisService.setByKey("test1","hello 2");
-	}
-	
-	@Cacheable("user")
-	@RequestMapping("/rGet")
-	public String get() {
-		return redisService.getByKey("test1");
-	} 
-	
-	@CacheEvict("user")
-	@RequestMapping("/rDel")
-	public void del() {
-		
-	}
 }
